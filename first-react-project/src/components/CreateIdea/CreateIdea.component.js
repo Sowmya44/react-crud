24c6a0c4c629fa49e64452c4d96dc2c96462faee
Rahat Khanna <rahat_khanna@apple.com>
2018-12-31 13:46:35 +0530

    C#2 Full form controlls

--- first-react-project/src/components/CreateIdea/CreateIdea.component.js
+++ first-react-project/src/components/CreateIdea/CreateIdea.component.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
+import './CreateIdea.css';
 
 class CreateIdea extends Component {
     constructor(props) {
       super(props);
-      this.state = { title: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({ 
-          title: event.target.value
-        });
+      this.idea = {};
     }
   
     handleSubmit(event) {
-      alert('A title was submitted: ' + this.state.title);
+      const newIdea = {
+          title: this.idea.title.value,
+          name: this.idea.name.value,
+          abstract: this.idea.abstract.value,
+          user_name: this.idea.user_name.value,
+          user_email: this.idea.user_email.value
+      };
+      alert('An idea was submitted:' + JSON.stringify(newIdea));
       event.preventDefault();
     }
   
@@ -24,11 +24,53 @@ class CreateIdea extends Component {
       return (
         <form onSubmit={this.handleSubmit}>
         <h2>Create New Idea</h2>
-          <label>
-            Title:
-            <input type="text" value={ this.state.title } onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
+        <div className="create-form">
+            <div className="form-row">
+                <label>
+                    Title:
+                    <input 
+                     type="text"
+                     placeholder="Enter title for Idea"
+                     ref={node => this.idea.title = node} />
+                </label>
+            </div>
+            <div className="form-row">
+                <label>
+                    Team Name:
+                    <input type="text"
+                     placeholder="Enter Team Name"
+                     ref={node => this.idea.name = node} />
+                </label>
+            </div>
+            <div className="form-row">
+                <label>
+                    Abstract:
+                    <textarea 
+                     placeholder="Enter Abtract Here"
+                     ref={node => this.idea.abstract = node} />
+                </label>
+            </div>
+            <div className="form-row">
+                <label>
+                    Your Name:
+                    <input type="text"
+                     placeholder="Enter Leader Name"
+                     ref={node => this.idea.user_name = node} />
+                </label>
+            </div>
+            <div className="form-row">
+                <label>
+                    Your Email:
+                    <input type="email"
+                     required
+                     placeholder="Enter Email"
+                     ref={node => this.idea.user_email = node} />
+                </label>
+            </div>
+            <div className="form-row">
+                <input type="submit" value="Submit" />
+            </div>
+        </div>
         </form>
       );
     }
